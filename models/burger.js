cffe97fd7f08770768935b13c03f4a90228dbc91
Liang Cheng <liangcheng6@gmail.com>
2017-05-25 13:50:46 -0400

    sequelized burger

---
+++ models/burger.js
@@ -0,0 +1,32 @@
+// Dependencies
+// =============================================================
+
+// This may be confusing but here Sequelize (capital) references the standard library
+var Sequelize = require("sequelize");
+// sequelize (lowercase) references our connection to the DB.
+var sequelize = require("../config/connection.js");
+
+// Creates a "Chirp" model that matches up with DB
+var burger = sequelize.define("burger", {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  burger_name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  devoured:{
+    type:Sequelize.BOOLEAN,
+    defaultValue: false
+  }
+}, {
+  timestamps: false
+});
+
+// Syncs with DB
+burger.sync();
+
+// Makes the Chirp Model available for other files (will also create a table)
+module.exports = burger;

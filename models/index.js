decb7cab29f95b04d839e8d7e7bc4f29c3f78d41
Liang Cheng <liangcheng6@gmail.com>
2017-05-24 21:54:54 -0400

    update

--- models/index.js
+++ models/index.js
@@ -8,12 +8,14 @@ var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
-console.log("environment: " + env)
+console.log("environment: " + env);
+
+var sequelize;
 
 if (config.use_env_variable) {
-    var sequelize = new Sequelize(process.env[config.use_env_variable]);
+    sequelize = new Sequelize(process.env[env]);
 } else {
-    var sequelize = new Sequelize(config.database, config.username, config.password, config);
+    sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
